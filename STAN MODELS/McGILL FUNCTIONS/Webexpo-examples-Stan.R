6a6677bee90af1c658ed61f889cb341c151e1148
Jérôme Lavoué <jerome.lavoue@umontreal.ca>
2024-08-27 15:32:37 -0400

    [STAN MODELS] Initialization of the webexpo STAN library.v0.25 SEG stan models done, SEG wrapping TBD

--- STAN MODELS/McGILL FUNCTIONS/Webexpo-examples-Stan.R
+++ STAN MODELS/McGILL FUNCTIONS/Webexpo-examples-Stan.R
@@ -18,35 +18,39 @@ source('https://raw.githubusercontent.com/webexpo/webexpo_r_lib/master/STAN%20MO
 source('https://raw.githubusercontent.com/webexpo/webexpo_r_lib/master/STAN%20MODELS/McGILL%20FUNCTIONS/SEG-uninformative-stan.R')
 source('https://raw.githubusercontent.com/webexpo/webexpo_r_lib/master/STAN%20MODELS/McGILL%20FUNCTIONS/SEG-informedMean-stan.R')
 
+# --- compiling the models -------------------------------------------------------------------------
+
+stan.models.list <- list()
+
+f <- 'https://raw.githubusercontent.com/webexpo/webexpo_r_lib/master/STAN%20MODELS/MODELS/seg_uninformative.stan'
+
+stan.models.list <- augment.stan.models.list(stan.models.list, stan.file = f)
+
+f <- 'https://raw.githubusercontent.com/webexpo/webexpo_r_lib/master/STAN%20MODELS/MODELS/seg_informedvar.stan'
+
+stan.models.list <- augment.stan.models.list(stan.models.list, f)
+
+compiled.models.list(stan.models.list)
+
 
 # --- Uninformative / normal / no ME -----------------------------------------------------------------------
 
 # compiling and saving the model
 
-f <- 'https://raw.githubusercontent.com/webexpo/webexpo_r_lib/master/STAN%20MODELS/MODELS/SEG_uninformative.stan'
-code <- readLines(f)
-stan.model.uninformative <- stan_model(model_code=code)
-saveRDS(stan.model.uninformative,"SEG_stan_model_uninformative.RDS")
 
 y <- c(6, 7.2, 5.4, 6.08)
 o <- SEG.uninformative.stan(y, outcome.is.logNormally.distributed=FALSE,
-                            models.folder=stan.folder)
+                            models.list=stan.models.list)
 
 plot(o$mu, o$sigma, type='p', pch='.')
 
 
 # --- InformedVar -------------------------------------------------------------------------
 
-f <- 'https://raw.githubusercontent.com/webexpo/webexpo_r_lib/master/STAN%20MODELS/MODELS/SEG_informedVar.stan'
-code <- readLines(f)
-stan.model.informedVar <- stan_model(model_code=code)
-saveRDS(stan.model.informedVar,"SEG_stan_model_informedVar.RDS")
-
-
 
 y <- c(6, 7.2, 5.4, 6.08)
 o <- SEG.informedvar.stan(y, outcome.is.logNormally.distributed=FALSE,
-                          models.folder=stan.folder)
+                          models.list=stan.models.list)
 
 plot(o$mu, o$sigma, type='p', pch='.')
 
